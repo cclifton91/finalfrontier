cb7dd4cdbc1fb93477832a60afbb6cd2f35795c1
Camille <devcclifton@gmail.com>
2025-03-02 21:09:24 -0600

    added dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import alpinejs from '@astrojs/alpinejs';
+
+import sitemap from '@astrojs/sitemap';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import icon from 'astro-icon';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [alpinejs(), sitemap(), icon()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
